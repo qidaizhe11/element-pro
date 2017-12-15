753cbd45e2d0e390b43b310bb9042cde072843f4
Panfeng <qidaizhe11@gmail.com>
2017-12-15 22:33:52 +0800

    ListItem, actions, 仍未发现理想方案

---
+++ src/components/List/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ep-list-item">
+    <slot name="meta"></slot>
+    <div class="ep-list-item-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+
+    }
+  }
+})
+</script>
+
