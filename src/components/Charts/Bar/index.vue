5714b4e496aa4e3b3dac73729f7e2924e79eb98e
Panfeng <qidaizhe11@gmail.com>
2018-01-23 15:01:38 +0800

    Charts/Bar, 启用autoHideXLabels属性

--- src/components/Charts/Bar/index.vue
+++ src/components/Charts/Bar/index.vue
@@ -38,100 +38,105 @@ export default Vue.extend({
     }
   },
   data() {
-    const { color, data } = this
-    const options = {
-      grid: {
-        left: 0,
-        right: 0,
-        top: 10,
-        bottom: 5,
-        containLabel: true
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'line',
-          lineStyle: {
-            width: 0
+    return {
+      autoHideXLabels: false
+    }
+  },
+  computed: {
+    options(): any {
+      const { color, data, autoHideXLabels } = this
+      return {
+        grid: {
+          left: 0,
+          right: 0,
+          top: 10,
+          bottom: 5,
+          containLabel: true
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            lineStyle: {
+              width: 0
+            }
+          },
+          formatter: (paramsList: any) => {
+            const colorSpan = (color: string) => {
+              return `<span style="display:inline-block;margin-right:5px;border-radius:10px;width:8px;height:8px;background-color:${color};border: 1px solid #fff;"></span>`
+            }
+            return paramsList.map((item: any) => {
+              console.log('echarts, formatter, item:', item)
+              return `${colorSpan(item.color)}${item.value[0]}: ${
+                item.value[1]
+              }`
+            })
+          },
+          confine: true,
+          textStyle: {
+            fontSize: 12
+          },
+          padding: [10, 5]
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisLabel: {
+              show: !autoHideXLabels,
+              color: '#797979',
+              margin: 12
+            },
+            axisTick: {
+              show: !autoHideXLabels,
+              alignWithLabel: true
+            },
+            axisLine: {
+              show: !autoHideXLabels,
+              lineStyle: {
+                color: '#bebebe'
+              }
+            },
+            axisPointer: {
+              status: 'hide'
+            },
+            data: data.map(item => {
+              return item.x
+            })
           }
-        },
-        formatter: (paramsList: any) => {
-          const colorSpan = (color: string) => {
-            return `<span style="display:inline-block;margin-right:5px;border-radius:10px;width:8px;height:8px;background-color:${color};border: 1px solid #fff;"></span>`
-          }
-          return paramsList.map((item: any) => {
-            console.log('echarts, formatter, item:', item)
-            return `${colorSpan(item.color)}${item.value[0]}: ${item.value[1]}`
-          })
-        },
-        confine: true,
-        textStyle: {
-          fontSize: 12
-        },
-        padding: [10, 5]
-      },
-      xAxis: [
-        {
-          type: 'category',
+        ],
+        yAxis: {
+          show: true,
           axisLabel: {
-            show: true,
-            color: '#797979',
-            margin: 12
+            color: '#797979'
           },
           axisTick: {
-            show: true,
-            alignWithLabel: true
+            show: false
           },
           axisLine: {
+            show: false
+          },
+          splitLine: {
             show: true,
             lineStyle: {
-              color: '#bebebe'
+              type: 'dotted'
             }
-          },
-          axisPointer: {
-            status: 'hide'
-          },
-          data: data.map(item => {
-            return item.x
-          })
-        }
-      ],
-      yAxis: {
-        show: true,
-        axisLabel: {
-          color: '#797979'
-        },
-        axisTick: {
-          show: false
-        },
-        axisLine: {
-          show: false
-        },
-        splitLine: {
-          show: true,
-          lineStyle: {
-            type: 'dotted'
           }
-        }
-      },
-      series: [
-        {
-          type: 'bar',
-          itemStyle: {
-            normal: {
-              color: color
-            }
-          },
-          barWidth: '50%',
-          data: data.map(item => {
-            return [item.x, item.y]
-          })
-        }
-      ]
-    }
-    return {
-      options,
-      autoHideXLabels: false
+        },
+        series: [
+          {
+            type: 'bar',
+            itemStyle: {
+              normal: {
+                color: color
+              }
+            },
+            barWidth: '50%',
+            data: data.map(item => {
+              return [item.x, item.y]
+            })
+          }
+        ]
+      }
     }
   },
   mounted() {
