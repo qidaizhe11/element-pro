32be3d9dcdbdd1c3c007ac52d6a7a2305539b34d
Panfeng <qidaizhe11@gmail.com>
2018-03-28 21:29:01 +0800

    Dashboard/Monitor, 实时交易情况，初步效果

--- src/views/Dashboard/Monitor/index.vue
+++ src/views/Dashboard/Monitor/index.vue
@@ -14,7 +14,34 @@
                 :total="numeral(124543233).format('0,0')"
               />
             </el-col>
+            <el-col :sm="6" :xs="24">
+              <number-info
+                sub-title="销售目标完成率"
+                total="92%"
+              />
+            </el-col>
+            <el-col :sm="6" :xs="24">
+              <number-info
+                sub-title="活动剩余时间"
+                :total="numeral(124543233).format('0,0')"
+              />
+            </el-col>
+            <el-col :sm="6" :xs="24">
+              <number-info
+                sub-title="每秒交易总额"
+                suffix="元"
+                :total="numeral(234).format('0,0')"
+              />
+            </el-col>
           </el-row>
+          <div class="map-chart">
+            <el-tooltip content="等待后期实现" placement="top">
+              <img
+                src="https://gw.alipayobjects.com/zos/rmsportal/HBWnDEUXCnGnGrRfrpKa.png"
+                alt="map"
+              />
+            </el-tooltip>
+          </div>
         </el-card>
       </el-col>
     </el-row>
@@ -23,7 +50,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { Row, Col, Card } from 'element-ui'
+import { Row, Col, Card, Tooltip } from 'element-ui'
 import * as numeral from 'numeral'
 
 import { yuan } from 'components/Charts'
@@ -32,6 +59,7 @@ import NumberInfo from 'components/NumberInfo'
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Card)
+Vue.use(Tooltip)
 Vue.use(NumberInfo)
 
 export default Vue.extend({
@@ -47,5 +75,22 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+@import '~theme/theme.scss';
 
+.map-chart {
+  padding-top: 24px;
+  height: 457px;
+  text-align: center;
+  img {
+    display: inline-block;
+    max-width: 100%;
+    max-height: 437px;
+  }
+}
+
+@media screen and (max-width: $screen-lg) {
+  .map-chart {
+    height: auto;
+  }
+}
 </style>
