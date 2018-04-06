2c4c7489440d1d2f4d288da0f92b3a9b350a787b
Panfeng <qidaizhe11@gmail.com>
2018-04-06 09:46:13 +0800

    Profile/BasicProfile, descriptionlist相关，初步完善

--- src/views/Profile/BasicProfile/index.vue
+++ src/views/Profile/BasicProfile/index.vue
@@ -14,6 +14,17 @@
         <description term="销售单号">1234123421</description>
         <description term="子订单">3214321432</description>
       </description-list>
+      <description-list
+        size="large"
+        title="用户信息"
+        :style="{marginBottom: '32px'}"
+      >
+        <description term="用户姓名">付小小</description>
+        <description term="联系电话">18100000000</description>
+        <description term="常用快递">菜鸟仓储</description>
+        <description term="取货地址">浙江省杭州市西湖区万塘路18号</description>
+        <description term="备注">无</description>
+      </description-list>
     </el-card>
   </page-header-layout>
 </template>
