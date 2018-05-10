2660a35ce3fda8b11b0fe2e253a6813227231922
Panfeng <qidaizhe11@gmail.com>
2018-05-10 22:44:13 +0800

    views/List/BasicList, 查询表格，开始实现

---
+++ src/views/List/BasicList/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <page-header-layout
+    title="查询表格"
+    v-bind="$attrs"
+  >
+    <div class="standard-list">
+      <el-card>
+        
+      </el-card>
+    </div>
+  </page-header-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Card } from 'element-ui'
+
+import PageHeaderLayout from 'layouts/PageHeaderLayout/index.vue'
+
+Vue.use(Card)
+
+export default Vue.extend({
+  components: {
+    PageHeaderLayout
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
